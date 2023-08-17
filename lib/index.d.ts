c6cc0402414383b787fe290deb7a68aa25280861
Lê Hoàng <80310280+lehoangnd@users.noreply.github.com>
2023-08-17 14:37:37 +0700

    Update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -30,6 +30,7 @@ export interface PhoneInputProps {
   layout?: "first" | "second";
   filterProps?: CountryFilterProps;
   countryPickerProps?: any;
+  disableCountry?: boolean;
 }
 export interface PhoneInputState {
   code: CallingCode | undefined;
